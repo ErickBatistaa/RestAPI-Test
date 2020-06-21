7fa2f924db53f58b5458a043a49354a40dc67b07
Erick Santos <erick.santos@negocieonline.com.br>
2020-06-21 20:10:04 -0300

    Ajust PersonService

---
+++ RestAPI-Test/RestAPI-Test/Services/PersonService.cs
@@ -0,0 +1,71 @@
+﻿using RestAPI_Test.Models;
+using RestAPI_Test.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RestAPI_Test.Services
+{
+    public class PersonService : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+
+
+        public Person FindById(long id)
+        {
+            return new Person 
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Erick",
+                LastName = "Batista",
+                Address = "São Paulo - SP - Brasil",
+                Gender = "Male"
+            };
+        }
+
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " + i,
+                LastName = "Person Last Name " + i,
+                Address = "Some Address " + i,
+                Gender = "Some Gender " + i
+            };
+        }
+    }
+}
--- RestAPI-Test/RestAPI-Test/Startup.cs
+++ RestAPI-Test/RestAPI-Test/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestAPI_Test.Services;
+using RestAPI_Test.Services.Interfaces;
 
 namespace RestAPI_Test
 {
@@ -26,6 +28,7 @@ namespace RestAPI_Test
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IPersonService, PersonService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
